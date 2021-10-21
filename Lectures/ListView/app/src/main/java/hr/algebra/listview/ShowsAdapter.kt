0d35e4280513359f5d6931206857aab7733d6f27
Luka Cabraja <luka@cabraja.eu>
2021-10-21 19:46:43 +0200

    comment

--- Lectures/ListView/app/src/main/java/hr/algebra/listview/ShowsAdapter.kt
+++ Lectures/ListView/app/src/main/java/hr/algebra/listview/ShowsAdapter.kt
@@ -1,6 +1,7 @@
 package hr.algebra.listview
 
 import android.content.Context
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +12,10 @@ import android.widget.TextView
 class ShowsAdapter(context: Context, shows: List<Show>) : ArrayAdapter<Show>(context, 0, shows) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        // we're reusing the previous view object to prevent from unnecessary inflating
+        val view = convertView ?: LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
         val show = getItem(position)
-
+        // but we're not reusing these, so the count of newly generated objects has onyl fallen from 3 to 2 in this example
         val ivShow = view.findViewById<ImageView>(R.id.ivShow)
         val tvShow = view.findViewById<TextView>(R.id.tvShow)
 
