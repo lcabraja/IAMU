4872cf32599116687441b23c4ed6b040d391e603
Luka Cabraja <luka@cabraja.eu>
2021-11-11 19:32:34 +0100

    added registration for activit result

--- Lectures/Intents/app/src/main/java/hr/algebra/intents/MainActivity.kt
+++ Lectures/Intents/app/src/main/java/hr/algebra/intents/MainActivity.kt
@@ -1,13 +1,26 @@
 package hr.algebra.intents
 
+import android.app.Instrumentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import hr.algebra.intents.databinding.ActivityMainBinding
+
+const val OUTPUT_PARAM = "hr.algebra.intents.output.param"
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val startActivityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (RESULT_OK == it.resultCode) {
+            var nrVowels = it.data?.getIntExtra(OUTPUT_PARAM, -1)
+            Toast.makeText(this, nrVowels.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         binding.btnCount.setOnClickListener {
             with(Intent(this, CountVowelsActivity::class.java)) {
                 putExtra(INPUT_PARAM, binding.etInput.text.toString().trim())
-                startActivity(this)
+                startActivityForResult.launch(this)
             }
         }
     }
