20a0d35c0edeb290a424c953d7161a8757d25186
Luka Cabraja <luka@cabraja.eu>
2022-01-04 20:01:40 +0100

    last exercise commit
    gradle broke

--- Exercises/Nasa/app/src/main/java/hr/algebra/nasa/HostActivity.kt
+++ Exercises/Nasa/app/src/main/java/hr/algebra/nasa/HostActivity.kt
@@ -2,6 +2,11 @@ package hr.algebra.nasa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import hr.algebra.nasa.databinding.ActivityHostBinding
 
 class HostActivity : AppCompatActivity() {
@@ -13,5 +18,42 @@ class HostActivity : AppCompatActivity() {
         )
         binding = ActivityHostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initHamburgerMenu()
+        initNavigation()
+    }
+
+    private fun initHamburgerMenu() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setIcon(R.drawable.ic_menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                toggleDrawer()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun exitApp() {
+        AlertDialog.Builder(this).apply {
+            setTitle()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (!binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        } else {
+            binding.drawerLayout.closeDrawers()
+        }
+    }
+
+    private fun initNavigation() {
+        var navControler = Navigation.findNavController(this, R.id.navHostFragment)
+        NavigationUI.setupWithNavController(binding.navigationView, navControler)
     }
 }
---
+++ Exercises/Nasa/app/src/main/java/hr/algebra/nasa/ItemsFragment.kt
@@ -0,0 +1,24 @@
+package hr.algebra.nasa
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ItemsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ItemsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_items, container, false)
+    }
+
+}
